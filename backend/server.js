2c6ea204c2782c0c91dcc6f7cd8b08cad5b185a6
hp <mohitgkp464@gmail.com>
2025-07-09 14:53:42 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://connect-sphere-12jq.vercel.app'],
     methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials: true
 }));
