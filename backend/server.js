31e316362979f81802cfbac61c3519a61dc24f40
hp <mohitgkp464@gmail.com>
2025-07-09 15:13:45 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const startServer = async () => {
 
     const io = new Server(server, {
         cors: {
-            origin: 'http://localhost:5173',
+            origin: ['http://localhost:5173', 'https://connect-sphere-red.vercel.app'],
             methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
             credentials: true
         }
